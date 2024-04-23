fbf511d02ab5758087f5a244130746cdf0d14e77
vidc <vid@tuta.com>
2024-04-23 19:16:59 +0200

    Added a 8 block maximum reach.

--- Client/menus/nameChooser/nameChooser.hpp
+++ Client/menus/nameChooser/nameChooser.hpp
@@ -6,9 +6,9 @@
 
 class NameChooser : public gfx::Scene {
 private:
-    ConfigFile config; // Declare config as a member variable
-    gfx::Button random_button; // Declare random_button as a member variable
-    std::vector<gfx::Button*> buttons; // Declare buttons as a member variable
+    ConfigFile config;
+    gfx::Button random_button;
+    std::vector<gfx::Button*> buttons;
 
     gfx::Button back_button, join_button;
     gfx::Sprite choose_name_title;
@@ -22,9 +22,5 @@ private:
     void render() override;
     void stop() override;
 public:
-    // void init();
-    // bool onKeyUp(gfx::Key key);
-    // void render();
-    // void stop();
     NameChooser(BackgroundRect* menu_back, Settings* settings, std::string server_ip) : gfx::Scene("NameChooser"), menu_back(menu_back), settings(settings), server_ip(std::move(server_ip)) {}
 };
